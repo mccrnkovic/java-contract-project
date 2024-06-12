81f615132d26f58248262a4b1ab90bf410e8c195
mcrnkovic <mcrnkovic@autozubak.hr>
2024-06-12 21:39:34 +0200

    spring project initialized

---
+++ crnkovic/src/main/java/com/omega_project/crnkovic/CrnkovicApplication.java
@@ -0,0 +1,13 @@
+package com.omega_project.crnkovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrnkovicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrnkovicApplication.class, args);
+	}
+
+}
---
+++ crnkovic/src/test/java/com/omega_project/crnkovic/CrnkovicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega_project.crnkovic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrnkovicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
