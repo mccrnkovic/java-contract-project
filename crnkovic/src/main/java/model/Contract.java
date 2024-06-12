a4b7cddfa765a8356891835dae885a9aa583976d
mcrnkovic <mcrnkovic@autozubak.hr>
2024-06-12 23:14:21 +0200

    Contract class persistance annotations

--- crnkovic/src/main/java/model/Contract.java
+++ crnkovic/src/main/java/model/Contract.java
@@ -1,15 +1,21 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
 @Data
+@Entity
 public class Contract {
-    String id;
-    String customer;
-    String number;
-    Date paymentDate;
-    Date deliveryDueDate;
-    String status;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String customer;
+    private String number;
+    private Date paymentDate;
+    private Date deliveryDueDate;
+    private String status;
 }
