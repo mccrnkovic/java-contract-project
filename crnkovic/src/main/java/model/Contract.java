ccd197825ae9923d4e1d9ef5903ea00fbe7a76fc
mcrnkovic <mcrnkovic@autozubak.hr>
2024-06-12 22:38:28 +0200

    added Contract model

---
+++ crnkovic/src/main/java/model/Contract.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+public class Contract {
+    String id;
+    String customer;
+    String number;
+    Date paymentDate;
+    Date deliveryDueDate;
+    String status;
+
+}
