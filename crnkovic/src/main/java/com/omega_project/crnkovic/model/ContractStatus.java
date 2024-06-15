b04163a4d6ef07d6ccb21c12f0cf40892f121a8d
mcrnkovic <mcrnkovic@autozubak.hr>
2024-06-15 17:22:10 +0200

    added isAllowedTransition check on enum

--- crnkovic/src/main/java/com/omega_project/crnkovic/model/ContractStatus.java
+++ crnkovic/src/main/java/com/omega_project/crnkovic/model/ContractStatus.java
@@ -1,5 +1,8 @@
 package com.omega_project.crnkovic.model;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum ContractStatus {
     DELIVERED(false),
     ORDERED(new ContractStatus[]{DELIVERED}, true),
@@ -14,6 +17,13 @@ public enum ContractStatus {
 
     ContractStatus(Boolean isActive) {
         this.isActive = isActive;
+    }
+
+    public Boolean isAllowedTransition(ContractStatus contractStatus) {
+        Optional<ContractStatus> allowedTransitionOptional = Arrays.stream(this.allowedTransitions)
+                .filter(allowed -> allowed == contractStatus).findAny();
+
+        return allowedTransitionOptional.isPresent();
     }
 
 }
